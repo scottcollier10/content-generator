21117ed24a4cf391ca6294db3b8ef580fcb17c1b
Scott Collier <scottcollier@me.com>
2026-02-16 10:27:29 -0600

    feat: add data models for Slack commands, brand context, and variations

---
+++ application/models/__init__.py
@@ -0,0 +1,17 @@
+from .schemas import (
+    SlackCommand,
+    BrandContext,
+    EmailVariation,
+    GenerationRequest,
+    GenerationResult,
+    HubSpotDraft,
+)
+
+__all__ = [
+    "SlackCommand",
+    "BrandContext",
+    "EmailVariation",
+    "GenerationRequest",
+    "GenerationResult",
+    "HubSpotDraft",
+]
